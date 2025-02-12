d86aaf5a5f6016c62675a9d60ab1dc15013bf7cb
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-11 21:17:35 -0500

    Fixed errors and added comments to Task 1

--- cc_8.js
+++ cc_8.js
@@ -1,7 +1,7 @@
 // Task 1 - Employee Salary Calculation
 function calculateSalary(baseSalary, bonus, taxRate) {
-    netSalary = (baseSalary + bonus) - (baseSalary * taxRate)
-    return `Net Salary: $${netSalary.toFixed(2)}`;
+    netSalary = (baseSalary + bonus) - (baseSalary * taxRate);// Calculates netsalary
+    return `Net Salary: $${netSalary.toFixed(2)}`;// Returns the net salary
 };
 console.log(calculateSalary(5000, 500, 0.1));// Expected output: "Net Salary: $5000.00"
-console.logcalculateSalary(7000, 1000, 0.15);// Expected output: "Net Salary: $6950.00"
+console.log(calculateSalary(7000, 1000, 0.15));// Expected output: "Net Salary: $6950.00"

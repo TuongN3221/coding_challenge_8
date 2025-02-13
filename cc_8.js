337356eed08b6454d1544370f8dc1502846e18ac
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-12 19:37:10 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,13 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 };
 console.log(calculateSalary(5000, 500, 0.1));// Expected output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15));// Expected output: "Net Salary: $6950.00"
+
+// Task 2 - Product Price After Discount
+function calculateDiscount(price, discountRate) {
+    finalPrice = price - (price * discountRate)// Calculates the final price
+    return `Final Price: $${finalPrice.toFixed(2)}`
+
+};// Function to calculate the discount for the final price
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
+

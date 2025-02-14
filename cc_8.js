d5c1875f7f07de0a8c32a002fef90e62072ad60f
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-13 21:19:34 -0500

    Task 6 -  Identifying Large Transactions

--- cc_8.js
+++ cc_8.js
@@ -41,6 +41,14 @@ function calculateLoanPayment(principal, rate, time) {
 console.log(calculateLoanPayment(1000, 0.05, 2)); // Expected output: "Total Payment: $1100.00"
 console.log(calculateLoanPayment(5000, 0.07, 3)); // Expected output: "Total Payment: $6050.00"
 
+// Task 6 - Identifying Large Transactions
+let transactions = [200, 1500, 3200, 800, 2500];
+
+function filterLargeTransactions(transactions, filterFunction) {
+    let filterTransactions = transactions.filter(filterFunction)
+    console.log(`Filtered Transactions: ${filterTransactions}`);
+};
+filterLargeTransactions(transactions, amount => amount > 1000);// Expected output: [1500, 3200, 2500]
 
 
 // Task 7 - Shopping Cart Tracker
